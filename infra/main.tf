db8878d04e07057ce55cbe8cdd50e553268f9772
Zhu Zhong <zhongzhu@outlook.com>
2023-09-15 23:32:59 +0800

    fix bug: private ssh key's file permission should be 600 instead of 644

--- infra/main.tf
+++ infra/main.tf
@@ -9,23 +9,27 @@ terraform {
 
 provider "tls" {}
 
-# output "public_key" {
-#   value = format("%s %s", tls_private_key.ssh-key.public_key_openssh, "zhongzhu@mylinux")
-# #   sensitive=true
-# }
-
-
 resource "tls_private_key" "ssh_key_pair" {
   algorithm   = "ED25519"
 }
 
 resource "null_resource" "generate_ssh_key" {
   provisioner "local-exec" {
-    command = "echo '${tls_private_key.ssh_key_pair.private_key_openssh}' > ~/.ssh/id_ed25519"
+    command = "echo '${tls_private_key.ssh_key_pair.private_key_openssh} ubuntu@mylinux' > ~/.ssh/id_ed25519"
+  }
+}
+
+resource "null_resource" "change_private_key_file_permission" {
+  depends_on = [ null_resource.generate_ssh_key ]
+
+  provisioner "local-exec" {
+    command = "chmod 600 ~/.ssh/id_ed25519"
   }
 }
 
 resource "null_resource" "generate_ssh_pub_key" {
+  depends_on = [ null_resource.generate_ssh_key ]
+
   provisioner "local-exec" {
     command = "echo '${tls_private_key.ssh_key_pair.public_key_openssh}' > ~/.ssh/id_ed25519.pub"
   }
